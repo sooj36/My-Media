0abe614f4f8f3000032b03a1f3894360b6bd8dac
bora park <jemini9595@gmail.com>
2023-09-27 22:22:52 +0900

    [Fix] : 메인주석처리

--- app/src/main/java/com/example/my_media/main/MainActivity.kt
+++ app/src/main/java/com/example/my_media/main/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.my_media.main
 import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
 import com.example.my_media.BuildConfig
 import com.example.my_media.R
@@ -11,47 +10,48 @@ import com.example.my_media.databinding.ActivityMainBinding
 import com.example.my_media.home.HomeFragment
 import com.example.my_media.mypage.MyVideoFragment
 import com.example.my_media.search.SearchFragment
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.common.api.Scope
+//import com.google.android.gms.auth.api.signin.GoogleSignIn
+//import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+//import com.google.android.gms.common.api.ApiException
+//import com.google.android.gms.common.api.Scope
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-    private val googleAuthLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-        if (result.resultCode == Activity.RESULT_OK) {
-            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
-            try {
-                val account = task.getResult(ApiException::class.java)
-                account.idToken?.let { initView(it) }
-            } catch (e: ApiException) {
-                Log.e("GoogleSignIn", "Google Sign-In failed: ${e.statusCode}, message: ${e.message}")
-            }
-        }
-    }
+//    private val googleAuthLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+//        if (result.resultCode == Activity.RESULT_OK) {
+//            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+//            try {
+//                val account = task.getResult(ApiException::class.java)
+//                account.idToken?.let { initView(it) }
+//            } catch (e: ApiException) {
+//                Log.e("GoogleSignIn", "Google Sign-In failed: ${e.statusCode}, message: ${e.message}")
+//            }
+//        }
+//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        requestGoogleLogin()
+//        requestGoogleLogin()
+        initView("")
     }
 
-    private fun requestGoogleLogin() {
-        val googleSignInOption = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(BuildConfig.GOOGLE_CLIENT_ID)
-//            .requestServerAuthCode(BuildConfig.GOOGLE_CLIENT_ID)
-            .requestEmail()
-            .requestScopes(Scope("https://www.googleapis.com/auth/youtube.force-ssl"))
-//            .requestScopes(Scope(YouTubeScopes.YOUTUBE_READONLY))
-            .build()
-        val googleSignInClient = GoogleSignIn.getClient(this, googleSignInOption)
-
-        googleSignInClient.signOut()
-        googleAuthLauncher.launch(googleSignInClient.signInIntent)
-    }
+//    private fun requestGoogleLogin() {
+//        val googleSignInOption = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+//            .requestIdToken(BuildConfig.GOOGLE_CLIENT_ID)
+////            .requestServerAuthCode(BuildConfig.GOOGLE_CLIENT_ID)
+//            .requestEmail()
+//            .requestScopes(Scope("https://www.googleapis.com/auth/youtube.force-ssl"))
+////            .requestScopes(Scope(YouTubeScopes.YOUTUBE_READONLY))
+//            .build()
+//        val googleSignInClient = GoogleSignIn.getClient(this, googleSignInOption)
+//
+//        googleSignInClient.signOut()
+//        googleAuthLauncher.launch(googleSignInClient.signInIntent)
+//    }
 
     private fun initView(accessToken: String) = with(binding) {
         val homeFragment = HomeFragment.newInstance(accessToken)
