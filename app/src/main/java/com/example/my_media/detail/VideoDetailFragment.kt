26018747f32ed660bab9fbae1adc4388482f8bf0
bora park <jemini9595@gmail.com>
2023-09-29 05:31:03 +0900

    [Fix] : 좋아요한 리스트 여러개 추가하기위해 livedata mutable로 변경

--- app/src/main/java/com/example/my_media/detail/VideoDetailFragment.kt
+++ app/src/main/java/com/example/my_media/detail/VideoDetailFragment.kt
@@ -1,24 +1,27 @@
 package com.example.my_media.detail
 
-import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import coil.load
 import com.example.my_media.R
 import com.example.my_media.databinding.FragmentVideoDetailBinding
 import com.example.my_media.home.popular.HomePopularModel
 import com.example.my_media.main.MainSharedViewModel
+import com.example.my_media.mypage.MyVideoViewModel
+import com.example.my_media.util.showToast
+import kotlin.math.log
 
 class VideoDetailFragment : Fragment() {
     companion object {
         fun newInstance(item: HomePopularModel) = VideoDetailFragment().apply {
             arguments = Bundle().apply {
-                putParcelable("item", item)//객체전달
+                putParcelable("item", item)
             }
         }
     }
@@ -38,48 +41,40 @@ class VideoDetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initView()
-        //넘겨온 아이템객체받기
         val item: HomePopularModel? = arguments?.getParcelable("item")
-        val videoId = item?.txtTitle ?: return
-
-        binding.apply {
-            titleArea.text = item?.txtTitle
-            desArea.text = item?.txtDescription
-        }
-        item?.imgThumbnail?.let {
-            binding.thumnailArea.load(it){
-                error(R.drawable.test)
-            }
-        }
-
-
-        binding.sharedBtn.setOnClickListener {
-            val sendIntent: Intent = Intent().apply {
-                action = Intent.ACTION_SEND
-                putExtra(Intent.EXTRA_TEXT, "나의 친구들에게 공유하세요!")
-                type = "text/plain"
-            }
-            val shareIntent = Intent.createChooser(sendIntent, null)
-            startActivity(shareIntent)
-        }
-
-        binding.likeBtn.run  {//apply보단 run이 간지
-            setImageResource(if (sharedViewModel.getLikeStatus(videoId)) R.drawable.ic_like else R.drawable.ic_mtlike)
-
-            setOnClickListener {
-                item.isLiked = !item.isLiked
-                val currentStatus = !sharedViewModel.getLikeStatus(videoId)
-                sharedViewModel.updateLikeStatus(videoId, currentStatus)
-                sharedViewModel.toggleLikeItem(item)//가독성때문에 with로 안묶음
-                setImageResource(if (currentStatus) R.drawable.ic_like else R.drawable.ic_mtlike)
-
-            }
+        item?.let {
+            initViewModel(it)
         }
     }
 
-    private fun initView() = with(binding) {
+    private fun initViewModel(item: HomePopularModel) {
+        item.isLiked = sharedViewModel.getLikeStatus(item.txtTitle)
 
+        binding.apply {
+            titleArea.text = item.txtTitle
+            binding.desArea.text = item.txtDescription
+            binding.thumnailArea.load(item.imgThumbnail) {
+                error(R.drawable.test)
+            }
+        }
+        updateLikeButtonUI(item.isLiked)
+
+        binding.likeBtn.setOnClickListener {
+            val isLiked = sharedViewModel.getLikeStatus(item.txtTitle)
+            val newItem = item.copy(isLiked = !isLiked)
+            sharedViewModel.toggleLikeItem(newItem)
+            if (isLiked) {
+                context?.showToast("좋아요 리스트에서 제거 되었습니다", Toast.LENGTH_LONG)
+            } else
+                context?.showToast("좋아요 리스트에 추가 되었습니다", Toast.LENGTH_LONG)
+            updateLikeButtonUI(newItem.isLiked)
+        }
+    }
+
+    private fun updateLikeButtonUI(isLiked: Boolean) {
+        binding.likeBtn.setImageResource(
+            if (isLiked) R.drawable.ic_like else R.drawable.ic_mtlike
+        )
     }
 
     override fun onDestroyView() {
