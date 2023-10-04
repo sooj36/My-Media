1dcc35fcbf17ce691c727959a8fcd35d415990e3
sinw212 <sinw212@gmail.com>
2023-10-04 23:03:12 +0900

    [Bug] HomeFragment 재선택 후 칩버튼 클릭 시 앱 종료 이슈 해결

--- app/src/main/java/com/example/my_media/home/HomeFragment.kt
+++ app/src/main/java/com/example/my_media/home/HomeFragment.kt
@@ -1,12 +1,11 @@
 package com.example.my_media.home
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
+import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.my_media.R
@@ -14,7 +13,6 @@ import com.example.my_media.databinding.FragmentHomeBinding
 import com.example.my_media.detail.VideoDetailFragment
 import com.example.my_media.home.popular.HomePopularListAdapter
 import com.example.my_media.home.subscribe.HomeSubscribeListAdapter
-import com.example.my_media.search.SearchFragment
 import com.example.my_media.util.UserManager
 
 class HomeFragment : Fragment() {
@@ -25,8 +23,9 @@ class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
 
+    private var videoCategoryId: String? = null
 
-    private val viewModel: HomeViewModel by viewModels() {
+    private val viewModel: HomeViewModel by activityViewModels() {
         HomeViewModelFactory()
     }
 
@@ -73,9 +72,6 @@ class HomeFragment : Fragment() {
         }
 
         val accessToken = UserManager.getAccessToken()
-        viewModel.getSubscribeList("Bearer $accessToken") //구독 리스트 불러오기
-
-        viewModel.getPopularVideo("Bearer $accessToken", "0")
 
         val chips = listOf(
             R.id.chip_all to "0",
@@ -85,16 +81,16 @@ class HomeFragment : Fragment() {
             R.id.chip_comedy to "23"
         )
 
-        var videoCategoryId = ""
-        chipGroup.setOnCheckedChangeListener { group, checkedId ->
-            for (i in chips.indices) {
-                if (checkedId == chips.get(i).first) {
-                    videoCategoryId = chips.get(i).second
-                    Log.d("sooj", "chips click ${videoCategoryId}")
-                }
+        viewModel.getSubscribeList("Bearer $accessToken") //구독 리스트 불러오기
+        if(videoCategoryId.isNullOrBlank()) {
+            viewModel.getPopularVideo("Bearer $accessToken","0") //인기 동영상 불러오기
+        }
+
+        chipGroup.setOnCheckedChangeListener { _, checkedId ->
+            if(checkedId != -1) {
+                videoCategoryId = chips.find { it.first == checkedId }?.second
+                viewModel.getPopularVideo("Bearer $accessToken", videoCategoryId ?: "0")
             }
-            viewModel.getPopularVideo("Bearer $accessToken", videoCategoryId)
-            Log.d("sooj", " test : $videoCategoryId ")
         }
     }
 
